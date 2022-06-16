70bdef82a84ee45438305aba1e1e193ba07366d9
rinezzzz <103594891+rinezzzz@users.noreply.github.com>
2022-06-16 22:00:42 +0700

    Add files via upload

---
+++ public/models/admin.js
@@ -0,0 +1,12 @@
+const Admin = [
+    {
+      id: '1',
+      username: 'admin0001',
+      password: 'admin0001',
+    },
+    {
+      id: '2',
+      username: 'admin0002',
+      password: 'admin0002',
+    },
+  ];
