07690b3bf78bafc0f6cb8646d3cf051f2b284a64
rinezzzz <103594891+rinezzzz@users.noreply.github.com>
2022-06-16 21:59:19 +0700

    Update Orders.js

--- public/models/Orders.js
+++ public/models/Orders.js
@@ -2,17 +2,11 @@ const orders = [
     {
       id: '1',
       customers_id: '1001',
-      delivery_address_id: '1111',
-      startdate: Date,
-      enddate: Date,
-      status: 'On Shipping'
+      product_id: '1111',
     },
     {
       id: '2',
       customers_id: '1002',
-      delivery_address_id: '1112',
-      startdate: Date,
-      enddate: Date,
-      status: 'On Shipping'
+      product_id: '1112',
     },
   ];
